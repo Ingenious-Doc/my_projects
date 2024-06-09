d64561db3377e8d7495931b202a25f7c58e6269f
Ingenious-Doc <dshakhsheer@gmail.com>
2024-06-09 16:01:20 +0400

    updating the files and the cut tool functions

--- cut tool/cut.py
+++ cut tool/cut.py
@@ -1,12 +1,34 @@
+import re
 the_command=input("Enter the command")
 
 
-def command_prompter(my_command):
-    command = my_command.split(" ")
-    field_no = []
-    for it in command:
-        if  it[0]=="-":
-
+def command_prompter(input_command):
+    command=input_command.split("|")
+    my_commands=[]
+    for split_commands in command:
+        my_commands.append(split_commands)
+    fields=[]
+    delimiter=""
+    file=""
+    lines=""
+    commands=[]
+    for func in my_commands:
+        if '-f"' in func:
+            for string in func:
+                
+        else:
+            function=func.split(" ")
+            function.remove("")
+            commands.append(function.pop(0))
+            for item in function:
+                if item[0:2]=="-f":
+                    fields=item[2:].split(",")
+                elif item[0:2]=="-d":
+                    delimiter=item[2]
+                elif item[0:2]=="-n":
+                    lines=item[2]
+                else:
+                    file=item
 
 
 def tab_file_parser(file):
@@ -19,4 +41,6 @@ def tab_file_parser(file):
     return matrix
 
 
+def field_picker(matrix,fields):
+    pass
 command_prompter(the_command)
